acda90ae9920b5d60b96140e7739e5ca6e5ccd0b
Miguel Amaral <118134009+miguelamaral254@users.noreply.github.com>
2023-12-05 21:25:37 -0300

    final

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,7 @@ public class Main {
     private String nomeTitular;
     private double saldo;
 
+    // Construtor
     public Main(int numero, String nomeTitular, double depositoInicial) {
         this.numero = numero;
         this.nomeTitular = nomeTitular;
@@ -13,16 +14,20 @@ public class Main {
         exibirDadosConta();
     }
 
+    // deposit method 
     public void deposito(double valor) {
         saldo += valor;
         System.out.println("Depósito de R$ " + valor + " realizado com sucesso!");
         exibirDadosConta();
     }
 
+    // withdraw method
     public void retirar(double valor) {
-        if (saldo >= valor) {
-            saldo -= valor;
+        double valorSaque = valor + 5.0; //taxa de saque(faz o L)
+        if (saldo >= valorSaque) {
+            saldo -= valorSaque;
             System.out.println("Saque de R$ " + valor + " realizado com sucesso!");
+            System.out.println("Taxa de saque: R$ 5.00");
         } else {
             System.out.println("Saldo insuficiente para realizar o saque.");
             System.out.println("Pedido de saque maior do que o saldo em conta.");
@@ -30,6 +35,7 @@ public class Main {
         exibirDadosConta();
     }
 
+    // Account data method
     public void exibirDadosConta() {
         System.out.println("Número da Conta: " + numero);
         System.out.println("Titular: " + nomeTitular);
@@ -40,6 +46,7 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        // Request account info
         System.out.print("Informe o número da conta: ");
         int numeroConta = scanner.nextInt();
         scanner.nextLine();
@@ -56,10 +63,12 @@ public class Main {
             depositoInicial = scanner.nextDouble();
         }
 
+        // create account
         Main conta = new Main(numeroConta, nomeTitular, depositoInicial);
 
         int opcao;
         do {
+            // Show menu
             System.out.println("Menu:");
             System.out.println("1. Depósito");
             System.out.println("2. Saque");
@@ -68,6 +77,7 @@ public class Main {
             System.out.print("Escolha uma opção: ");
             opcao = scanner.nextInt();
 
+            // Switch 
             switch (opcao) {
                 case 1:
                     System.out.print("Informe o valor do depósito: R$ ");
@@ -85,7 +95,8 @@ public class Main {
                 default:
                     System.out.println("Opção inválida. Tente novamente.");
             }
-        } while (opcao != 3);
+        } while (opcao != 3); //kill program
+
         scanner.close();
     }
 }
